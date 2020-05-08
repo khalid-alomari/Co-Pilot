9117c2ef211c026dacc77409ad5874be508bb51c
Khalid Alomari <62272563+khalido394@users.noreply.github.com>
2020-05-08 13:18:54 -0700

    Update SN.java

--- Co-Pilot code/JAVA-XML-Kotlin /SN.java
+++ Co-Pilot code/JAVA-XML-Kotlin /SN.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class SN extends AppCompatActivity {
-
+    // Open new connection 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
