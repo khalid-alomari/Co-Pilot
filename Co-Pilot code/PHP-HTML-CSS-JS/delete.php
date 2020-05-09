436d7203927bce55b61760d45b6d1b56ae282c09
Khalid Alomari <khalido394@gmail.com>
2020-05-08 17:01:28 -0700

    Adding Comments
    Adding Comments for the deletion, and checking status process

--- Co-Pilot code/PHP-HTML-CSS-JS/delete.php
+++ Co-Pilot code/PHP-HTML-CSS-JS/delete.php
@@ -3,9 +3,10 @@
 require "init.php";
 $User_ID = $_GET["id"];
 
-
+// Delete user!   
 $sql_query = "delete from users where User_ID like '$User_ID';";
 
+// checking the status
 if(mysqli_query($con,$sql_query)){
 	header("refresh:1; url=main.php");
 } else {
